8eb6f94173a47c14151a99fbd883d4f7e67de5a0
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2016-04-08 13:06:14 +0200

    aded edgeR and DESeq2

--- R-scripts/install_required_libraries.R
+++ R-scripts/install_required_libraries.R
@@ -1,3 +1,17 @@
 ################################################################
 ## Install libraries required for this project
 
+source("https://bioconductor.org/biocLite.R")
+
+if (!require(edgeR)) {
+  biocLite("edgeR") ## Install package only if it cannot be loaded (because not installed yet)
+  library(edgeR)
+}
+
+if (!require(DESeq2)) {
+  biocLite("DESeq2") ## Install package only if it cannot be loaded (because not installed yet)
+  library(DESeq2)
+}
+
+
+
