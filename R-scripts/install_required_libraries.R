875094de929fa99447d39137fecd84b557910bd4
Mustafa ABUELQUMSAN <m.qumsan@gmail.com>
2016-10-05 15:02:16 +0200

    implemented function GeomMean

--- R-scripts/install_required_libraries.R
+++ R-scripts/install_required_libraries.R
@@ -3,13 +3,14 @@
 
 source("https://bioconductor.org/biocLite.R")
 
+
 if (!require(edgeR)) {
   biocLite("edgeR") ## Install package only if it cannot be loaded (because not installed yet)
   library(edgeR)
 }
 
 if (!require(DESeq2)) {
-  biocLite("DESeq2") ## Install package only if it cannot be loaded (because not installed yet)
+biocLite("DESeq2") ## Install package only if it cannot be loaded (because not installed yet)
   library(DESeq2)
 }
 
